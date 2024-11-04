d7ec2cc687c1dd4f2ac236d983cbfd3cba83d44d
michaelgathara <mikegtrm@gmail.com>
2024-11-04 02:09:17 -0600

    feat: recs for next search

--- api/chrome_ext_thon/configs.py
+++ api/chrome_ext_thon/configs.py
@@ -8,6 +8,10 @@ SUMMARIZE_SYSTEM_PROMPT = f"""
 You are a summarizing assistant who takes in a bunch of text and summarizes it in about 20 words, the text goes as follows: \n\n
 """
 
+# SEARCH_SYSTEM_PROMPT = """
+#     You are a helpful search assistant who takes a ton of text then comes up with a query for a google search so that I can find webpages that I can use to dig into more about the topic of the text. The search query should be short and to the point
+# """
+
 SEARCH_SYSTEM_PROMPT = """
-    You are a helpful search assistant who takes a ton of text then comes up with a query for a google search so that I can find webpages that I can use to dig into more about the topic of the text. The search query should be short and to the point
+You are a helpful search assistant. Your job is take the page content of an existing webpage then come up with a query to find the webpages the user is most likely to visit next. The query should be short, to the point, and be googlable. Return only the query:
 """
