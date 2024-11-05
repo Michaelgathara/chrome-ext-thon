2dc55989be073f3442504a0c259ecc92427b3a84
Jayden Pyles <jpylesbusiness@gmail.com>
2024-11-04 18:48:59 -0600

    chore: prompt engineer

--- api/chrome_ext_thon/configs.py
+++ api/chrome_ext_thon/configs.py
@@ -16,5 +16,7 @@ Each summary should either be a single paragraph or a list of bullet points. The
 # """
 
 SEARCH_SYSTEM_PROMPT = """
-You are a helpful search assistant. Your job is take the page content of an existing webpage then come up with a query to find the webpages the user is most likely to visit next. The query should be short, to the point, and be googlable. Return only the query:
+You are a helpful search assistant. Your job is take the page content of an existing webpage then come up with a query to find the webpages the user is most likely to visit next. 
+This should be a prediction of what the user is looking for next, while content may be relevant to the page, it should not be the same as the page title or description.
+The query should be short, to the point, and be googlable. Return only the query:
 """
