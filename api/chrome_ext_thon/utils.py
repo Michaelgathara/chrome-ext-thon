83a0b5a25293563df6a29d2754c35cb868c395a2
Jayden Pyles <jpylesbusiness@gmail.com>
2024-12-03 16:49:19 -0600

    fix: remove calls to genai

--- api/chrome_ext_thon/utils.py
+++ api/chrome_ext_thon/utils.py
@@ -1,7 +1,6 @@
 # pyright: reportUnknownMemberType=false
 import os, requests, logging
 from typing import cast
-import google.generativeai as genai
 from api.chrome_ext_thon.google_utils import (
     SearchResult,
     search,
@@ -26,31 +25,31 @@ logging.basicConfig(
 
 LOG = logging.getLogger(__name__)
 
-genai.configure(api_key=GEMINI_KEY)
+# genai.configure(api_key=GEMINI_KEY)
 
 
-async def gemini(
-    user_prompt: str, use_case: int = 1, model_name: str = "gemini-1.5-flash-latest"
-):
-    """
-    Models avail:
-    Gemini 1.5 Pro: gemini-1.5-pro-002
-    Gemini 1.5 Flash: gemini-1.5-flash-002
-    Gemini 1.5 Flash-8B: gemini-1.5-flash-8b
-    Gemini 1.0 Pro: gemini-1.0-pro-002
-    Text Embedding Model: text-embedding-004
-    AQA (Answer Quality Assessment): aqa
-    """
-    try:
-        model = genai.GenerativeModel(model_name)
-        system = SUMMARIZE_SYSTEM_PROMPT if use_case == 1 else SEARCH_SYSTEM_PROMPT
-        response = model.generate_content(
-            [{"role": "user", "parts": [system + "\n\n" + user_prompt]}]
-        )
-        return response.text
-    except Exception as e:
-        LOG.error(f"Error in Gemini processing: {e}")
-        return user_prompt
+# async def gemini(
+#     user_prompt: str, use_case: int = 1, model_name: str = "gemini-1.5-flash-latest"
+# ):
+#     """
+#     Models avail:
+#     Gemini 1.5 Pro: gemini-1.5-pro-002
+#     Gemini 1.5 Flash: gemini-1.5-flash-002
+#     Gemini 1.5 Flash-8B: gemini-1.5-flash-8b
+#     Gemini 1.0 Pro: gemini-1.0-pro-002
+#     Text Embedding Model: text-embedding-004
+#     AQA (Answer Quality Assessment): aqa
+#     """
+#     try:
+#         model = genai.GenerativeModel(model_name)
+#         system = SUMMARIZE_SYSTEM_PROMPT if use_case == 1 else SEARCH_SYSTEM_PROMPT
+#         response = model.generate_content(
+#             [{"role": "user", "parts": [system + "\n\n" + user_prompt]}]
+#         )
+#         return response.text
+#     except Exception as e:
+#         LOG.error(f"Error in Gemini processing: {e}")
+#         return user_prompt
 
 
 async def google_search(query: str):
